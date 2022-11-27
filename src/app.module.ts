37bf92e8ddf4acbb62b379f4f859326ce24bb435
Santiago Saavedra <santiagosaavedra98@gmail.com>
2022-11-27 12:55:11 -0500

    addded user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CharacterModule } from './character/character.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CharacterModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,98 @@
+import { PrismaService } from './../prisma.service';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = await this .prismaService.user.create({
+        data: createUserDto
+      })
+
+      return user
+    } catch (error) {
+      console.log(error)
+
+      if (error.code === 'P2002') {
+        throw new BadRequestException('Username already taken, please selecta a new one')
+      }
+
+      throw new InternalServerErrorException('Something went wrong, read server logs')
+    }
+  }
+
+  async findAll() {
+    try {
+      const users = await this.prismaService.user.findMany()
+  
+      return users.map(user => {
+        delete user.password
+  
+        return user
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async findOne(id: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id
+      },
+      select: {
+        id: true,
+        username: true,
+        createdAt: true,
+        updatedAt: true,
+      }
+    })
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return user
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.findOne(id)
+
+    try {
+      const user = await this.prismaService.user.update({
+        where: {
+          id
+        },
+        data: {
+          ...updateUserDto
+        }
+      })
+
+      return user
+    } catch (error) {
+      console.log({error})
+
+      if (error.code === 'P2002') {
+        throw new BadRequestException('Username already exist')
+      }
+    }
+  }
+
+  async remove(id: number) {
+    await this.findOne(id)
+
+    try {
+      this.prismaService.user.delete({
+        where: {
+          id
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
